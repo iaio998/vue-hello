f8925e4a5d2ac39cdc432644a4887d013725f2dd
iaio998 <depalatisiacopo@gmail.com>
2023-10-27 15:06:19 +0200

    Fixed exercise with css

--- javascript/script.js
+++ javascript/script.js
@@ -5,10 +5,10 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      text: "Hello my friendz! :D",
-      image: "../img/meme.jpg",
+      text: "HELLO MY FRIENDZ! :D",
+      image: "img/meme.jpg",
       background:
-        "background-image: url(../img/background.jpg); background-repeat: no-repeat",
+        "background-image: url(img/background.jpg); background-repeat: no-repeat",
     };
   },
 }).mount("#app");
